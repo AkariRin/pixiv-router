c30a03ff81c5943ca5108d527dc569d56ec369f5
AkariRin <akaririn1028@gmail.com>
2022-08-15 22:48:01 +0800

    chore: update pwa config

--- vue.config.js
+++ vue.config.js
@@ -6,11 +6,21 @@ module.exports = defineConfig({
     workboxOptions: {
       skipWaiting: true,
     },
+    themeColor: "#00BFFF",
+    msTileColor: "#00BFFF",
     iconPaths: {
       appleTouchIcon: "img/icons/apple-touch-icon.png",
     },
     manifestOptions: {
       categories: ["tools"],
+      background_color: "#00BFFF",
+      icons: [
+        {
+          src: "img/icons/android-chrome-144x144.png",
+          sizes: "144x144",
+          type: "image/png",
+        },
+      ],
     },
   },
 });
